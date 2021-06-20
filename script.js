d3cf610c954891a15f1027436d01d020e4c7ae88
Forarc <forarc@hotmail.com>
2021-06-20 10:50:25 -0300

    criar comida

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -20,6 +24,11 @@ function criarCobrinha() {
     }
 }
 
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown', update);
 
 function update(event) {
@@ -29,12 +38,15 @@ function update(event) {
     if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
-
 function iniciarJogo() {
-
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x =0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -48,7 +60,7 @@ function iniciarJogo() {
 
     let newHead = {
         x: snakeX,
-        y: snake
+        y: snakeY
     }
 
     snake.unshift(newHead);
